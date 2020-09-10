ab53e2b56cde2d4a4752b378457f8f3540724f75
Glitch (hello-webpage) <none>
2020-09-10 21:43:43 +0000

    🌑🍲 Checkpoint ./index.html:21940894/1764 ./script.js:21940894/1188 ./script.js copy:21940894/4786 ./style.css:21940894/1740

--- script.js
+++ script.js
@@ -1,5 +1,28 @@
-/* If you're feeling fancy you can add interactivity 
-    to your site with Javascript */
+// TODO: load the dataset 
 
-// prints "hi" in the browser's dev tools console
-console.log("hi");
+
+fetch('https://s3.us-west-2.amazonaws.com/secure.notion-static.com/9d7f28c1-b3cb-41cc-b1ab-c42f5b62beeb/attractions.json?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAT73L2G45O3KS52Y5%2F20200910%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20200910T213804Z&X-Amz-Expires=86400&X-Amz-Signature=b48deac180278e879e1349b21b05d595093cb92df2acf0021dc4d24e8890d3e7&X-Amz-SignedHeaders=host&response-content-disposition=filename%20%3D%22attractions.json%22');
+
+function filterData(category) {
+  
+
+	/* **************************************************
+	 *
+	 * TODO: filter attractions by the selected category
+	 * TODO: filter top 5 attractions
+	 *
+	 * CALL THE FOLLOWING FUNCTION TO RENDER THE BAR-CHART:
+	 *
+	 * renderBarChart(data)
+	 *
+	 * - 'data' must be an array of JSON objects
+	 * - the max. length of 'data' is 5
+	 *
+	 * **************************************************/
+  
+  
+  
+}
+
+// TODO: Define an event listener for the dropdown menu
+//       Call filterData with the selected category
