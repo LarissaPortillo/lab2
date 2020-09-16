059ecbbe92ddd16ee1ce8e28eef0e092daa7451e
Glitch (hello-webpage) <none>
2020-09-16 03:52:47 +0000

    ⛺️🏢 Checkpoint ./script.js:21940894/481

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // TODO: load the dataset 
 
 
-fetch('https://s3.us-west-2.amazonaws.com/secure.notion-static.com/9d7f28c1-b3cb-41cc-b1ab-c42f5b62beeb/attractions.json?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAT73L2G45O3KS52Y5%2F20200910%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20200910T213804Z&X-Amz-Expires=86400&X-Amz-Signature=b48deac180278e879e1349b21b05d595093cb92df2acf0021dc4d24e8890d3e7&X-Amz-SignedHeaders=host&response-content-disposition=filename%20%3D%22attractions.json%22')
+fetch("./attractions.json")
   .then(response => response.json())
 function filterData(category) {
   
